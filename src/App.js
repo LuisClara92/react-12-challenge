06f42d92ee394f1bd4c7fc95fab05f06be271ad4
LuisClara92 <lc.sbg.92@gmail.com>
2020-11-14 12:27:07 +0000

    fix bug on tab

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
-import TabSelector from './TabSelector';
-import './App.css';
+import React, { Component } from "react";
+import TabSelector from "./TabSelector";
+import "./App.css";
 
 const Home = () => <p>This is the Home component!</p>;
 const About = () => <p>This challenge is about tabs!</p>;
 const Contact = () => <p>Feel free to contact us!</p>;
-const Error = () => <p>Something went <strong>wrong</strong>!</p>;
+const Error = () => (
+  <p>
+    Something went <strong>wrong</strong>!
+  </p>
+);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-  }
+export default class App extends Component {
+  state = {
+    activeId: "home",
+  };
 
-  getTabContent() {
-    switch(this.state.activeId) {
-      case 'home':
+  handleChangeTab = (event) => {
+    const buttonId = event.target.id;
+    this.setState({ activeId: buttonId });
+    // console.log(buttonId);
+  };
+
+  getTabContent = () => {
+    switch (this.state.activeId) {
+      case "home":
+        this.state = true;
         return <Home />;
+      case "about":
+        this.state = true;
+        return <About />;
+      case "contact":
+        this.state = true;
+        return <Contact />;
       default:
         return <Error />;
     }
-  }
+  };
 
   render() {
+    // console.log(this.getTabContent);
+
     return (
       <div className="App">
-        <TabSelector />
-        <div className="App-content">
-          {
-            this.getTabContent()
-          }
-        </div>
+        <TabSelector
+          handleChangeTab={this.handleChangeTab}
+          activeId={this.state.activeId}
+        />
+        <div className="App-content">{this.getTabContent()}</div>
       </div>
     );
   }
 }
-
-export default App;
--- src/TabSelector.js
+++ src/TabSelector.js
@@ -1,40 +1,33 @@
 import React, { Component } from 'react';
 
-class TabSelector extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-    this.handleChangeTab = this.handleChangeTab.bind(this);
-  }
-
-  handleChangeTab(event) {
-    const buttonId = event.target.id;
-    this.setState({ activeId: buttonId });
-  }
+export default class TabSelector extends Component {
+ 
+  // handleChangeTab = (event) => {
+  //   const buttonId = event.target.id;
+  //   this.setState({ activeId: buttonId });
+  // }
 
   render() {
-    const { activeId } = this.state;
+    const { activeId } = this.props;
     return (
       <div className="TabSelector">
         <button
           id="home"
-          onClick={this.handleChangeTab}
+          onClick={this.props.handleChangeTab}
           className={activeId === 'home' ? 'active' : ''}
         >
           Home
         </button>
         <button
           id="about"
-          onClick={this.handleChangeTab}
+          onClick={this.props.handleChangeTab}
           className={activeId === 'about' ? 'active' : ''}
         >
           About
         </button>
         <button
           id="contact"
-          onClick={this.handleChangeTab}
+          onClick={this.props.handleChangeTab}
           className={activeId === 'contact' ? 'active' : ''}
         >
           Contact
@@ -43,5 +36,3 @@ class TabSelector extends Component {
     );
   }
 }
-
-export default TabSelector;
